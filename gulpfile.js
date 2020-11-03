ea2a0feb6a831485bc5a7089022733c28aa110f7
Keshav Jain <keshav.jain2354@gmail.com>
2020-11-03 12:31:46 +0530

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
+    
 
 gulp.task('sass', function () {
     return gulp.src('./css/*.scss')
@@ -21,7 +30,7 @@ gulp.task('browser-sync', function () {
         './img/*.{png,jpg,gif}',
         './js/*.js'
     ];
-    
+
     browserSync.init(files, {
         server: {
             baseDir: "./"
@@ -32,4 +41,38 @@ gulp.task('browser-sync', function () {
 // Default task
 gulp.task('default', ['browser-sync'], function() {
     gulp.start('sass:watch');
+});
+
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function() {
+   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+    .pipe(flatmap(function(stream, file){
+        return stream
+        .pipe(usemin({
+            css: [ rev() ],
+            html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+            js: [ uglify(), rev() ],
+            inlinejs: [ uglify() ],
+            inlinecss: [ cleanCss(), 'concat' ]
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build',['clean'], function() {
+    gulp.start('copyfonts','imagemin','usemin');
 });
